7a0da3851d832ad0847b211d4ee08793148f7a07
Amin Mahrami <amin.mahrami@yahoo.com>
2022-12-10 23:38:24 +0100

    Day 10 - part two

--- src/2022/day-10/index.ts
+++ src/2022/day-10/index.ts
@@ -6,36 +6,61 @@ const input = nextLineParseString(rawInput).map((i) => {
 	return { instruction, value: parseInt(value ?? 0) };
 });
 
-function solutionOne() {
-	let result = 0;
-	let cycle = 0;
-	let signalStrength = 1;
+let result = 0;
+let cycle = 0;
+let signalStrength = 1;
+let monitor: string[][] = [];
 
-	function incrementResultIfNeeded() {
-		if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
-			result += cycle * signalStrength;
-		}
+for (let i = 0; i < 6; i++) {
+	const row = [];
+	for (let j = 0; j < 40; j++) {
+		row.push(" ");
 	}
-
-	input.forEach((line) => {
-		cycle++;
-		const { instruction, value } = line;
-		switch (instruction) {
-			case "noop":
-				incrementResultIfNeeded();
-				break;
-			case "addx":
-				incrementResultIfNeeded();
-				cycle++;
-				incrementResultIfNeeded();
-				signalStrength += value;
-				break;
-			default:
-				break;
-		}
-	});
-
-	return result;
+	monitor.push(row);
 }
 
-console.log("Solution 1: ", solutionOne()); // 13180
+function incrementResultIfNeeded() {
+	if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
+		result += cycle * signalStrength;
+	}
+}
+
+function drawIfNeeded() {
+	const CRTPosition = cycle % 40;
+	if (
+		[signalStrength - 1, signalStrength, signalStrength + 1].includes(
+			CRTPosition
+		)
+	) {
+		monitor[Math.floor(cycle / 40)][CRTPosition] = "#";
+	}
+}
+
+input.forEach((line) => {
+	drawIfNeeded();
+	cycle++;
+	const { instruction, value } = line;
+	switch (instruction) {
+		case "noop":
+			incrementResultIfNeeded();
+			break;
+		case "addx":
+			drawIfNeeded();
+			incrementResultIfNeeded();
+			cycle++;
+			incrementResultIfNeeded();
+			signalStrength += value;
+			break;
+		default:
+			break;
+	}
+});
+
+console.log("Solution 1: ", result); // 13180
+monitor.map((i) => console.log(i.join("").toString()));
+// #### #### ####  ##  #  #   ##  ##  ###
+// #       # #    #  # #  #    # #  # #  #
+// ###    #  ###  #    ####    # #  # ###
+// #     #   #    #    #  #    # #### #  #
+// #    #    #    #  # #  # #  # #  # #  #
+// #### #### #     ##  #  #  ##  #  # ###
