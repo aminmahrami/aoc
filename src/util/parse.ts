202894cfa117f33cddc812327bc89542d2cdfeb2
Amin Mahrami <amin.mahrami@yahoo.com>
2022-12-11 20:54:25 +0100

    Day 11 - part one

--- src/util/parse.ts
+++ src/util/parse.ts
@@ -1,29 +1,42 @@
 import { sum } from "./math";
 
 declare global {
-  interface Array<T> {
-    sortDesc(): Array<number>;
-    sortAsc(): Array<number>;
-    sum(): number;
-  }
+	interface Array<T> {
+		sortDesc(): Array<number>;
+		sortAsc(): Array<number>;
+		sum(): number;
+		multiply(): number;
+		getLast(): T | undefined;
+	}
 }
 
 export const nextLineParseString = (str: string): string[] => str.split("\n");
 
+export const twoNextLinesParseString = (str: string): string[] =>
+	str.split("\n\n");
+
 export const nextLineParseNumber = (str: string): number[] =>
-  str.split("\n").map(stringToNumber);
+	str.split("\n").map(stringToNumber);
 
 export const stringToNumber = (val: string | number | undefined): number =>
-  parseInt(`${val}`);
+	parseInt(`${val}`);
 
 Array.prototype.sortDesc = function (): number[] {
-  return this.sort((a, b) => b - a);
+	return this.sort((a, b) => b - a);
 };
 
 Array.prototype.sortAsc = function (): number[] {
-  return this.sort((a, b) => a - b);
+	return this.sort((a, b) => a - b);
 };
 
 Array.prototype.sum = function (): number {
-  return sum(this);
+	return sum(this);
+};
+
+Array.prototype.getLast = function () {
+	return this[this.length - 1];
+};
+
+Array.prototype.multiply = function () {
+	return this.reduce((t, c) => c * t);
 };
